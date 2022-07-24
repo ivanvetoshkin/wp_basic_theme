453f2f1d33475bc1df6663fbec630db8c3337db3
Ivan Vetoshkin <me@ivanvetoshkin.me>
2022-07-24 10:59:44 +0200

    Added pseudohover

--- functions.php
+++ functions.php
@@ -8,3 +8,10 @@ add_theme_support( 'automatic-feed-links' );
 add_theme_support('html5', array('comment-list', 'search-form', 'comment-form', 'gallery'));
 add_theme_support( 'post-thumbnails' );
 add_editor_style();
+
+
+/* Registering and enqueueing the script for links hover */
+function enqueue_hover() {
+    wp_enqueue_script( 'hover', get_template_directory_uri() . '/js/hover.js', array( 'jquery' ), null, 'in_footer' );
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_hover' );
