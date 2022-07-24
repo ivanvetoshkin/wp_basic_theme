8da741bc1bc84b160d3257e15c15d14c2c498f11
Ivan Vetoshkin <me@ivanvetoshkin.me>
2022-07-24 10:59:34 +0200

    Removed unused WP filters, added editor styles

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php 
+
+remove_filter('the_content', 'wptexturize');
+remove_filter('the_title', 'wptexturize');
+remove_filter('comment_text', 'wptexturize');
+
+add_theme_support( 'automatic-feed-links' );
+add_theme_support('html5', array('comment-list', 'search-form', 'comment-form', 'gallery'));
+add_theme_support( 'post-thumbnails' );
+add_editor_style();
