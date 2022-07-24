85918e63e9ef6e1ac5b54bae0bf5b2f29c7e2ab5
Ivan Vetoshkin <me@ivanvetoshkin.me>
2022-07-24 10:05:06 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html xml:lang="ru" lang="ru-RU" prefix="og: http://ogp.me/ns#">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  <title>
+    <?php echo wp_get_document_title(); ?>
+  </title>
+  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
+  <?php wp_head();
+  setlocale(LC_ALL, 'ru_RU'); ?>
+
+  <meta name="theme-color" content="">
+
+</head>
+
+
+<body <?php body_class(); ?>>
+
+<header role="banner">
+
+
+
+</header>
